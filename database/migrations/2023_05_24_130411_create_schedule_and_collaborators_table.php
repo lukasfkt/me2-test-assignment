ea69d8657c29b50069cbaf6b144356ca9b5aa451
Tutty <tuttytanaka@hotmail.com>
2023-05-29 17:36:22 -0300

    Adding phpmyadmin to docker and creating migrations

---
+++ database/migrations/2023_05_24_130411_create_schedule_and_collaborators_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100)->unique();
+            $table->timestamps();
+        });
+
+        Schema::create('collaborators', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('schedule_id')->constrained();
+            $table->string('name', 100)->unique();
+            $table->string('registration')->unique();
+            $table->string('cpf', 20)->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('collaborators');
+        Schema::dropIfExists('schedules');
+    }
+};
